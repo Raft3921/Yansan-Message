2f17b0f3efada613b4de75794a61c6aed3d437a8
Raft3921 <Raft3921@users.noreply.github.com>
2026-06-03 07:21:18 +0900

    Improve PWA notifications and unread badges

--- sw.js
+++ sw.js
@@ -8,15 +8,22 @@ self.addEventListener("activate", function (event) {
 
 self.addEventListener("notificationclick", function (event) {
   event.notification.close();
+  var data = event.notification.data || {};
+  var url = data.url || "./";
   event.waitUntil(
     self.clients.matchAll({ type: "window", includeUncontrolled: true }).then(function (clients) {
       for (var i = 0; i < clients.length; i += 1) {
         if ("focus" in clients[i]) {
-          return clients[i].focus();
+          return clients[i].focus().then(function (client) {
+            if (data.groupId && "postMessage" in client) {
+              client.postMessage({ groupId: data.groupId });
+            }
+            return client;
+          });
         }
       }
       if (self.clients.openWindow) {
-        return self.clients.openWindow(event.notification.data && event.notification.data.url || "./");
+        return self.clients.openWindow(url);
       }
       return undefined;
     })
